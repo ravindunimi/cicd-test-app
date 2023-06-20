707d9aedf2e86549b6b9a0197a56d54e0bd43ff7
ravindunimi <ravindu@nimidev.com>
2023-06-20 15:30:24 +0530

    new BE

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree", "userFour", "userFive"] });
+  res.json({ users: ["userOne", "userTwo", "userThree", "userFour", "userFive", "userSix"] });
 });
 
 app.listen(5000, () => {
