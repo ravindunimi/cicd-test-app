82eaaa73ebe38ca61601541b23cdaf55059aa2f5
ravindunimi <ravindu@nimidev.com>
2023-06-26 10:48:01 +0530

    Added new users

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ users: ["Francho", "Layla", "Peter", "Kevin"] });
+  res.json({ users: ["Belarik", "Layla", "Peter", "Kevin"] });
 });
 
 app.get("/members", (req, res) => {
-  res.json({ users: ["Ravind", "David", "Victor"] });
+  res.json({ users: ["Silvana", "David", "Victor"] });
 });
 
 app.listen(5000, () => {
